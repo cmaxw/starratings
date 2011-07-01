e088b688cc86bc4105f188a2bfa2ad0a1a5afe98
Charles Max Wood <chuck@teachmetocode.com>
2011-06-30 20:15:44 -0600

    set index to movies index page and styling for stars

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Starrating::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "movies#index"
 
   # See how all your routes lay out with "rake routes"
 
