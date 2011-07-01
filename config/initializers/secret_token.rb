c507635e3f37b6766edaaf7bd9802ef73d94538a
Charles Max Wood <chuck@teachmetocode.com>
2011-06-30 19:45:15 -0600

    original rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Starrating::Application.config.secret_token = '4b50f511def6575b4b1e657d6b345f4280a5a0329ee7e272e1a61870c29001adfead5243115f67ceae8e4fd6228635d805b2116fd663ee8edbf8efce8202dfd6'
